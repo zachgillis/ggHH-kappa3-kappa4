f78aaa09f96db866fcd02de1c65518fbd418c90a
Zach Gillis <45103920+zachgillis@users.noreply.github.com>
2023-12-16 12:13:48 -0600

    Update clean.py

--- clean.py
+++ clean.py
@@ -2,6 +2,8 @@
 
 import os
 
+WORKDIR = '/home/zachgillis/PWHG-7'
+
 def remove_files_with_extension(directory, extension):
     for filename in os.listdir(directory):
         if filename.endswith(extension):
@@ -13,7 +15,7 @@ def remove_files_with_extension(directory, extension):
                 print(f"Error while removing {file_path}: {e}")
 
 # Replace 'directory_path' with the path to your target directory
-directory_path = '/home/zachgillis/PWHG-5/ggHH-kappa3-kappa4'
+directory_path = os.path.join(WORKDIR, 'ggHH-kappa3-kappa4')
 
 remove_files_with_extension(directory_path, '.out')
 remove_files_with_extension(directory_path, '.err')
