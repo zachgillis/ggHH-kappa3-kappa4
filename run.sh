2b5ca5c36dd0300b46deaabe34e708de05aaa66c
Zach Gillis <45103920+zachgillis@users.noreply.github.com>
2024-11-05 14:37:00 -0600

    Update run.sh

--- run.sh
+++ run.sh
@@ -1,77 +1,19 @@
 #!/usr/bin/bash
 
-export WORKDIR=/home/zachgillis/PWHG-7
+export WORKDIR=/home/zachgillis/test110424_06
 
-cd ~
-
+export ALRB_CONT_RUNPAYLOAD="cd ..; cd ..; cd $WORKDIR/ggHH-kappa3-kappa4; source script.sh"
+export ALRB_CONT_PRESETUP="export WORKDIR=${WORKDIR}; export k3=${k3}; export k4=${k4}; export energy=${energy}; export current_directory=${current_directory}; export run_arg=${run_arg}"
+export ALRB_CONT_CMDOPTS=" --pwd=$WORKDIR/POWHEG-BOX-V2/ggHH -B $WORKDIR:$WORKDIR"
 export ATLAS_LOCAL_ROOT_BASE=/cvmfs/atlas.cern.ch/repo/ATLASLocalRootBase
-source ${ATLAS_LOCAL_ROOT_BASE}/user/atlasLocalSetup.sh
+source "${ATLAS_LOCAL_ROOT_BASE}/user/atlasLocalSetup.sh"
 
-current_directory=$4
+export k3=$1
+export k4=$2
+export energy=$3
+export current_directory=$4
+export run_arg=$5
 
 cd $WORKDIR/POWHEG-BOX-V2/ggHH
-
-setupATLAS
-lsetup "views LCG_98python3 x86_64-centos7-gcc8-opt"
-make -j5
-export LHAPDF_DATA_PATH=/cvmfs/sft.cern.ch/lcg/external/lhapdfsets/current:`lhapdf-config --datadir`
-
-k3=$1
-k4=$2
-
-kappa3=$(python - <<END
-import re
-
-def convert_m_to_negative(input_string):
-    def replace_m(match):
-        number = float(match.group(1))
-        return str(-1 * number)
-    
-    pattern = r'm(\d+(\.\d+)?)'
-    result = re.sub(pattern, replace_m, input_string)
-    return result
-
-print(convert_m_to_negative(str('$k3')))
-END
-)
-
-kappa4=$(python - <<END
-import re
-
-def convert_m_to_negative(input_string):
-    def replace_m(match):
-        number = float(match.group(1))
-        return str(-1 * number)
-    
-    pattern = r'm(\d+(\.\d+)?)'
-    result = re.sub(pattern, replace_m, input_string)
-    return result
-
-print(convert_m_to_negative(str('$k4')))
-END
-)
-
-energy=$3
-
-cd ../..
-cd ggHH-kappa3-kappa4
-cp config.yaml "${current_directory}/config_yaml.txt"
-cd ..
-cd POWHEG-BOX-V2/ggHH
-
-cp -r "testrun" "${current_directory}_kappa3 = ${kappa3}, kappa4 = ${kappa4}"
-cd "${current_directory}_kappa3 = ${kappa3}, kappa4 = ${kappa4}"
-
-sed -i "s/7000d0/${energy}d0/g" powheg.input-save
-
-sed -i "s/ch4       1.0/ch4       ${kappa4}/" powheg.input-save
-sed -i "s/chhh      1.0/chhh      ${kappa3}/" powheg.input-save
-
-./run.sh $5
-
-mkdir "$WORKDIR/ggHH-kappa3-kappa4/${current_directory}/kappa3 = ${kappa3}, kappa4 = ${kappa4}"
-
-find . -maxdepth 1 -type f -name "run-2*.log" -printf '%T+ %p\n' | sort -r | head -n 1 | cut -d' ' -f2- | xargs -I {} cp {} "$WORKDIR/ggHH-kappa3-kappa4/${current_directory}/kappa3 = ${kappa3}, kappa4 = ${kappa4}"
-
-cd ..
-ln -s "$PWD/${current_directory}_kappa3 = ${kappa3}, kappa4 = ${kappa4}" "$WORKDIR/ggHH-kappa3-kappa4/${current_directory}/kappa3 = ${kappa3}, kappa4 = ${kappa4}"
+setupATLAS -c centos7
+exit $?
