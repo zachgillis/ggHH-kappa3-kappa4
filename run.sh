bd118027db09ca2b7c92b0a9a977d4e9646ece23
Zach Gillis <45103920+zachgillis@users.noreply.github.com>
2024-11-08 16:16:54 -0600

    Update run.sh

--- run.sh
+++ run.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/bash
 
-export WORKDIR=/home/zachgillis/test110524
+export SCRATCHDIR=$pwd
 
 export k3=$2
 export k4=$3
@@ -8,12 +8,16 @@ export energy=$4
 export current_directory=$5
 export run_arg=$6
 
-export ALRB_CONT_RUNPAYLOAD="cd ..; cd ..; cd $WORKDIR/ggHH-kappa3-kappa4; source script.sh ${k3} ${k4} ${energy} ${current_directory} ${run_arg} ${WORKDIR}"
-# export ALRB_CONT_POSTSETUP="export WORKDIR=${WORKDIR}; export k3=${k3}; export k4=${k4}; export energy=${energy}; export current_directory=${current_directory}; export run_arg=${run_arg}"
-export ALRB_CONT_CMDOPTS=" --pwd=$WORKDIR/POWHEG-BOX-V2/ggHH -B $WORKDIR:$WORKDIR"
+export ALRB_CONT_RUNPAYLOAD="source script.sh ${k3} ${k4} ${energy} ${current_directory} ${run_arg}"
+export ALRB_CONT_CMDOPTS=" --pwd=`pwd`"
 export ATLAS_LOCAL_ROOT_BASE=/cvmfs/atlas.cern.ch/repo/ATLASLocalRootBase
 source "${ATLAS_LOCAL_ROOT_BASE}/user/atlasLocalSetup.sh"
 
-cd $WORKDIR/POWHEG-BOX-V2/ggHH
+svn checkout --username anonymous --password anonymous svn://powhegbox.mib.infn.it/trunk/POWHEG-BOX-V2
+cd POWHEG-BOX-V2
+mkdir ggHH
+git clone -b v1.0.0 git@github.com:philippwindischhofer/POWHEG_ggHH_cH4.git ./ggHH
+
+cd ..
 setupATLAS -c centos7
 exit $?
