f29858f48e6cf28a924cfb38417c64918300e697
Zach Gillis <45103920+zachgillis@users.noreply.github.com>
2024-11-08 16:16:27 -0600

    Update script.sh

--- script.sh
+++ script.sh
@@ -1,32 +1,17 @@
-echo $1
-echo $2
-echo $3
-echo $4
-echo $5
-echo $6
-
-
 export k3=$1
 export k4=$2
 export energy=$3
 export current_directory=$4
 export run_arg=$5
-export WORKDIR=$6
 
-cd $WORKDIR/POWHEG-BOX-V2/ggHH
+cd POWHEG-BOX-V2/ggHH
 
 lsetup "views LCG_98python3 x86_64-centos7-gcc8-opt"
 make -j5
 export LHAPDF_DATA_PATH=/cvmfs/sft.cern.ch/lcg/external/lhapdfsets/current:`lhapdf-config --datadir`
 
-cd ../..
-cd ggHH-kappa3-kappa4
-cp config.yaml "${current_directory}/config_yaml.txt"
-cd ..
-cd POWHEG-BOX-V2/ggHH
-
-cp -r "testrun" "${current_directory}_kappa3 = ${k3}, kappa4 = ${k4}"
-cd "${current_directory}_kappa3 = ${k3}, kappa4 = ${k4}"
+cp -r "testrun" "${current_directory}_kappa3_${k3}_kappa4_${k4}"
+cd "${current_directory}_kappa3_${k3}_kappa4_${k4}"
 
 sed -i "s/7000d0/${energy}d0/g" powheg.input-save
 
@@ -34,10 +19,3 @@ sed -i "s/ch4       1.0/ch4       ${k4}/" powheg.input-save
 sed -i "s/chhh      1.0/chhh      ${k3}/" powheg.input-save
 
 ./run.sh ${run_arg}
-
-mkdir "$WORKDIR/ggHH-kappa3-kappa4/${current_directory}/kappa3 = ${k3}, kappa4 = ${k4}"
-
-find . -maxdepth 1 -type f -name "run-2*.log" -printf '%T+ %p\n' | sort -r | head -n 1 | cut -d' ' -f2- | xargs -I {} cp {} "$WORKDIR/ggHH-kappa3-kappa4/${current_directory}/kappa3 = ${k3}, kappa4 = ${k4}"
-
-cd ..
-ln -s "$PWD/${current_directory}_kappa3 = ${k3}, kappa4 = ${k4}" "$WORKDIR/ggHH-kappa3-kappa4/${current_directory}/kappa3 = ${k3}, kappa4 = ${k4}"
